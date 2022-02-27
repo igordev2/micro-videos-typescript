3dab322cc5a89dfee76888a4949e3e875ccdc0e4
Igor de Souza Bezerra <igorsouzabezerra@outlook.com>
2022-02-26 22:35:45 -0300

    domain create category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -6,7 +6,11 @@ export type CategoryProperties = {
 }
 
 export class Category {
-  constructor (public readonly props: CategoryProperties) {}
+  constructor (public readonly props: CategoryProperties) {
+    this.description = this.props.description
+    this.is_active = this.props.is_active
+    this.props.created_at = this.props.created_at ?? new Date()
+  }
 
   get name() {
     return this.props.name
@@ -16,8 +20,16 @@ export class Category {
     return this.props.description
   }
 
+  private set description(description: string) {
+    this.props.description = description ?? null
+  }
+
   get is_active() {
     return this.props.is_active
+  }
+
+  private set is_active (is_active: boolean) {
+    this.props.is_active = is_active ?? true
   }
 
   get created_at() {
